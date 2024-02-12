d5dea0a15fae1a130a6678b7556e09711bb7d329
sidia <lobnasidia@gmail.com>
2024-02-12 13:55:16 +0100

    third commit

--- README.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main () {
-printf("bienvenue");
-return 0;
-}
---
+++ bienvenue.cpp
@@ -0,0 +1,7 @@
+// Affiche un message de bienvenue
+#include "fonction-bienvenue.h"
+int main()
+{
+afficherBienvenue();
+return 0;
+}
